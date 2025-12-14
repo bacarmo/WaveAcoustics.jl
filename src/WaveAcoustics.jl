15ad6afdb9156a45d840f0a56d05bf356d095115
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2025-12-13 22:13:26 -0300

    Organize module exports and includes in WaveAcoustics.jl

--- src/WaveAcoustics.jl
+++ src/WaveAcoustics.jl
@@ -1,5 +1,16 @@
 module WaveAcoustics
 
-# Write your package code here.
+using StaticArrays: SVector
+
+# Exports
+export CartesianMesh
+export Lagrange, Hermite
+export LeftRight, LeftRightBottomTop, LeftRightTop
+
+# Includes
+include("fe_families.jl")
+include("boundary_conditions.jl")
+include("mesh.jl")
+include("dof_map.jl")
 
 end
