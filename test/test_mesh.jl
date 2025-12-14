334090d5da43af156f4ac299f0094b6edf424f1b
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2025-12-13 22:10:28 -0300

    Add tests for CartesianMesh 1D and 2D functionality

---
+++ test/test_mesh.jl
@@ -0,0 +1,19 @@
+@testitem "CartesianMesh 1D" begin
+    using WaveAcoustics
+
+    mesh = CartesianMesh((0.0,), (1.0,), (10,))
+    @test mesh.pmin == (0.0,)
+    @test mesh.pmax == (1.0,)
+    @test mesh.Nx == (10,)
+    @test mesh.Δx == (0.1,)
+end
+
+@testitem "CartesianMesh 2D" begin
+    using WaveAcoustics
+
+    mesh = CartesianMesh((0.0, 0.0), (1.0, 2.0), (10, 20))
+    @test mesh.pmin == (0.0, 0.0)
+    @test mesh.pmax == (1.0, 2.0)
+    @test mesh.Nx == (10, 20)
+    @test mesh.Δx == (0.1, 0.1)
+end
