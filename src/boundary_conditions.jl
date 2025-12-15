f10437beaf42dbbc9301d06b474cdad3ab4cb953
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2025-12-14 23:48:52 -0300

    improve documentation

--- src/boundary_conditions.jl
+++ src/boundary_conditions.jl
@@ -5,6 +5,25 @@ Abstract supertype for domain sides with Dirichlet BCs.
 """
 abstract type DirichletSides end
 
-struct LeftRight <: DirichletSides end              # 1D: left and right
-struct LeftRightBottomTop <: DirichletSides end     # 2D: all 4 sides
-struct LeftRightTop <: DirichletSides end           # 2D: left, right, top
+"""
+    LeftRight <: DirichletSides
+
+Dirichlet boundary conditions imposed on the left and right sides of a 1D domain.
+"""
+struct LeftRight <: DirichletSides end
+
+"""
+    LeftRightBottomTop <: DirichletSides
+
+Dirichlet boundary conditions imposed on all four sides
+(left, right, bottom, and top) of a 2D domain.
+"""
+struct LeftRightBottomTop <: DirichletSides end
+
+"""
+    LeftRightTop <: DirichletSides
+
+Dirichlet boundary conditions imposed on the left, right, and top sides
+of a 2D domain.
+"""
+struct LeftRightTop <: DirichletSides end
