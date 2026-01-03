ea7af89e9d25e4a687c444805cc2946f063b72f0
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2026-01-02 23:52:41 -0300

    Refactor module exports and update dependencies in Project.toml and Manifest.toml

--- src/WaveAcoustics.jl
+++ src/WaveAcoustics.jl
@@ -1,17 +1,24 @@
 module WaveAcoustics
 
-using StaticArrays: SVector
+using StaticArrays: SVector, SMatrix, @SVector, @SMatrix
+using GaussQuadrature: legendre
 
 # Exports
-export CartesianMesh
+export PDECommonData, PDEInputData
+export manufactured_solution_case, zero_source_case
+export example1_manufactured, example1_zero_source
 export Lagrange, Hermite
-export LeftRight, LeftRightBottomTop, LeftRightTop
+export example1_manufactured, example1_zero_source
+export pde_solve
 
 # Includes
+include("pde_inputdata.jl")
+include("pde_solve.jl")
 include("fe_families.jl")
 include("boundary_conditions.jl")
 include("basis_functions.jl")
 include("mesh.jl")
 include("dof_map.jl")
+include("quadrature_setup.jl")
 
 end
