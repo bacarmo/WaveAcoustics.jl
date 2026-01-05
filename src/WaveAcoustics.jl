8eb546d1e06d09f32d422aff386ec0792a669540
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2026-01-05 19:58:36 -0300

    Add convergence test export and include in WaveAcoustics module

--- src/WaveAcoustics.jl
+++ src/WaveAcoustics.jl
@@ -2,18 +2,20 @@ module WaveAcoustics
 
 using StaticArrays: SVector, SMatrix, @SVector, @SMatrix
 using GaussQuadrature: legendre
+using Printf: @printf
 
 # Exports
 export PDECommonData, PDEInputData
 export manufactured_solution_case, zero_source_case
 export example1_manufactured, example1_zero_source
 export Lagrange, Hermite
-export example1_manufactured, example1_zero_source
 export pde_solve
+export convergence_test, print_convergence_table
 
 # Includes
 include("pde_inputdata.jl")
 include("pde_solve.jl")
+include("convergence_test.jl")
 include("fe_families.jl")
 include("boundary_conditions.jl")
 include("basis_functions.jl")
