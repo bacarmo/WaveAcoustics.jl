68eb0a68b6579b52efbaf1e51515a52cceceffe1
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2026-01-08 19:39:18 -0300

    Reorder include statements for improved organization

--- src/WaveAcoustics.jl
+++ src/WaveAcoustics.jl
@@ -13,14 +13,13 @@ export pde_solve
 export convergence_test, print_convergence_table
 
 # Includes
-include("pde_inputdata.jl")
-include("pde_solve.jl")
-include("convergence_test.jl")
 include("fe_families.jl")
 include("boundary_conditions.jl")
 include("basis_functions.jl")
 include("mesh.jl")
 include("dof_map.jl")
 include("quadrature_setup.jl")
-
+include("pde_inputdata.jl")
+include("pde_solve.jl")
+include("convergence_test.jl")
 end
